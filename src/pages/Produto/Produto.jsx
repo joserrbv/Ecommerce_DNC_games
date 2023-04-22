f5504c8e0463303711ce6bcbfbc6ce5db72c7544
joserrbv <118081152+joserrbv@users.noreply.github.com>
2023-04-22 15:54:06 -0400

    codigo do detalhe do produto e criação do modal carrinho para a proxima branch

--- src/pages/Produto/Produto.jsx
+++ src/pages/Produto/Produto.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import Nav from '../../components/Nav/Nav'
+import Footer from '../../components/Footer/Footer'
+import DetalhedoProduto from '../../components/DetalhedoProduto/DetalhedoProduto'
+import { useParams } from 'react-router-dom'
+import './index.scss'
 
-const Produto = () => {
+const Produto = ({ dados }) => {
+   const {produtoId} = useParams() ;
+   
+   const selectedProduto = dados.find(produto =>
+     produto.id == produtoId)
+  //  console.log(produtoId);
+
   return (
-    <div>Produto</div>
+    <div>
+      <Nav/>
+
+      <DetalhedoProduto dados={selectedProduto}/>
+
+            
+      <Footer/>
+
+
+      
+    </div>
   )
 }
 
-export default Produto
-// Comentário para criação da branch etapa_detalhe_do_produto
+export default Produto;

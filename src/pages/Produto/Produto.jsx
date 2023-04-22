666dfa9689d31c9b926b50ac32de0824fdb85f3b
joserrbv <118081152+joserrbv@users.noreply.github.com>
2023-04-22 17:53:16 -0400

    Revert "Etapa meu carrinho"

--- src/pages/Produto/Produto.jsx
+++ src/pages/Produto/Produto.jsx
@@ -1,29 +1,8 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
-import '../Produto/index.scss'
-import Nav from '../../components/Nav/Nav'
-import Footer from '../../components/Footer/Footer'
-import DetalhedoProduto from '../../components/DetalhedoProduto/DetalhedoProduto'
 
-const Produto = ({ dados }) => {
-   const {produtoId} = useParams() ;
-   
-   const selectedProduto = dados.find(produto =>
-     produto.id == produtoId)
-  //  console.log(selectedProduto);
-
+const Produto = () => {
   return (
-    <div>
-      <Nav/>
-
-      <DetalhedoProduto dados={selectedProduto}/>
-
-            
-      <Footer/>
-
-
-      
-    </div>
+    <div>Produto</div>
   )
 }
 
